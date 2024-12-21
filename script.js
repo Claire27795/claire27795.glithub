337719c0481d9c4354c049497604b6cc4ab0c8b6
Claire27795 <milkymine26@gmail.com>
2024-12-21 16:41:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("HelloWorld")
+let input = prompt("whatnumber is your grade?");
+let num = Number.parseInt(input);
+if(num>= 80){
+    console.log(You got Grade A);
+}
+else if(num >=70){
+    console.log("you got grade B");
+}
